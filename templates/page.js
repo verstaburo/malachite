f3c5078b8151ec42f638146b41aae8663d27b837
Allkrain <aukra25@yandex.ru>
2020-09-16 14:59:27 +0300

    Update 2020-09-16T11:53:16.625Z

--- templates/page.js
+++
@@ -1,9 +0,0 @@
-module.exports = pageName => `
-extends ../../layouts/default
-
-block head
-  - var pageTitle = '${pageName}'
-
-block content
-  p Страница: ${pageName}
-`;
--- templates/scss.js
+++
@@ -1,5 +0,0 @@
-module.exports = blockName => `
-.${blockName} {
-  $b: &;
-}
-`;
